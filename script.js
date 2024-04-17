d2dea505811a975a6532c38e327e54d547ab175d
Vincent Catalan <126637057+vinpcat@users.noreply.github.com>
2024-04-17 15:22:06 +0800

    added cursor script

---
+++ script.js
@@ -0,0 +1,33 @@
+const cursorTag= document.querySelector("div.cursors")
+const balls = cursorTag.querySelectorAll("div")
+
+let aimX = 0
+let aimY = 0
+
+balls.forEach((ball, index) => {
+    let currentX = 0
+    let currentY = 0
+
+    let speed = 0.3 - index * 0.015
+
+    const animate = function () {
+    currentX += (aimX - currentX) * speed
+    currentY += (aimY - currentY) * speed
+
+    ball.style.left = currentX + "px"
+    ball.style.top = currentY + "px"
+
+    requestAnimationFrame(animate)
+}
+
+animate()
+
+})
+
+
+document.addEventListener("mousemove", function (ev) {
+   aimX = ev.pageX
+   aimY = ev.pageY
+})
+
+
